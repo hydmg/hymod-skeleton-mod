af8ebbcdd0f53ca81174705feace5754691e0c13
Bilal Itani <bilalitani1@gmail.com>
2026-01-15 15:59:47 -0600

    Update with proper main class

--- src/main/java/com/example/skeleton/Main.java
+++ src/main/java/com/example/skeleton/Main.java
@@ -1,7 +1,25 @@
 package <PACKAGE>;
 
-public class Main {
-    public Main() {
-        System.out.println("Hello, Hytale!");
+import com.hypixel.hytale.server.core.plugin.JavaPlugin;
+import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+import javax.annotation.Nonnull;
+import java.util.logging.Logger;
+
+public class Main extends JavaPlugin {
+    private static final Logger LOGGER = Logger.getLogger("<MOD_NAME>");
+
+    public Main(@Nonnull JavaPluginInit init) {
+        super(init);
+        LOGGER.info("Main initialized");
+    }
+
+    @Override
+    protected void setup() {
+        LOGGER.info("Main setup");
+    }
+
+    @Override
+    protected void shutdown() {
+        LOGGER.info("Main shutdown");
     }
 }
